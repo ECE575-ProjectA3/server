c1d4ae3fc040fa529b20cf98c9ee65b7098496b9
Ryan Clarke <ryan@debvirt>
2015-04-14 23:55:01 -0400

    Add date/time filtering

--- complete/src/main/java/updatePackage/StoreData.java
+++ complete/src/main/java/updatePackage/StoreData.java
@@ -1,7 +1,7 @@
 package updatePackage;
 
 import java.sql.Statement;
-import java.sql.PreparedStatement;
+//import java.sql.PreparedStatement;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
@@ -9,6 +9,17 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class StoreData {
+	
+	//database field names
+	public static final String LATITUDE				= "latitude";
+	public static final String LONGITUDE			= "longitude";
+	public static final String DATETIME				= "dateTime";
+	public static final String SIGNALSTRENGTH		= "signalStrength";
+	public static final String DOWNLOADSPEED		= "downloadSpeed";
+	public static final String UPLOADSPEED			= "uploadSpeed";
+	public static final String WIFISIGNALSTRENGTH	= "wifiSignalStrength";
+	public static final String WIFIDOWNLOADSPEED	= "wifiDownloadSpeed";
+	public static final String WIFIUPLOADSPEED		= "wifiUploadSpeed";
 	
 	String url = "jdbc:mysql://universitystudenthousing.cji1dpedv3e5.us-east-1.rds.amazonaws.com:3306/csc540project1";
     String user = "";
@@ -45,7 +56,7 @@ public class StoreData {
 			query += cp.getDownloadSpeed() + "," + cp.getUploadSpeed() + ",";
 			query += cp.getWifiSignalStrength() + "," + cp.getWifiDownloadSpeed() + ",";
 			query += cp.getWifiUploadSpeed() + ",'";
-			query += cp.getTime() + "')";
+			query += cp.getDateTime() + "')";
 			
 			System.out.println(query);			
 		    
@@ -60,7 +71,10 @@ public class StoreData {
 		}		
 	}	
 	
-	public ArrayList<VisualParams> getData(String carrierName, String dataType) {
+	/*
+	 * Fetch filtered data from the specified table
+	 */
+	public ArrayList<VisualParams> getData(String carrierName, String dataType, String minDate, String maxDate, String minTime, String maxTime) {
 		
 		ArrayList<VisualParams> vpa = new ArrayList<VisualParams>(); 
 		
@@ -76,22 +90,34 @@ public class StoreData {
 			con = DriverManager.getConnection(url, user, password);
 		    st = con.createStatement();
 		    
-		    query = "SELECT * FROM " + carrierName;
+		    //filter results by date and time
+		    query = "SELECT * FROM "+carrierName+
+		    		" WHERE "+DATETIME+" > " + minDate + " AND "+DATETIME+" <= DATEADD(day,1," + maxDate + ")" +
+		    		" AND DATEPART(hh,"+DATETIME+") >= " + minTime +
+		    		" AND DATEPART(hh,"+DATETIME+") <= " + maxTime;
+		    
 		    System.out.println(query);
-		    ResultSet rs = st.executeQuery(query);	
+		    ResultSet rs = st.executeQuery(query);
 		    
 		    while (rs.next()) {
 		    	Number dataValue;
-		    	if(dataType=="signalStrength") {
+		    	if(dataType==SIGNALSTRENGTH || dataType==WIFISIGNALSTRENGTH) {
 		    		dataValue = rs.getInt(dataType);
 		    	} else {
 		    		dataValue = rs.getDouble(dataType);
 		    	}
-		    	vpa.add(new VisualParams(rs.getDouble("latitude"),rs.getDouble("longitude"),dataValue, dataType, rs.getString("time")));
+		    	vpa.add(new VisualParams(rs.getDouble(LATITUDE),rs.getDouble(LONGITUDE),
+		    			dataValue, dataType, rs.getString(DATETIME)));
 		    }
 		} catch (SQLException ex) {
 			System.out.println("Unable to perform query operation");
 		}
 		return vpa;
+	}
+	public ArrayList<VisualParams> getData(String carrierName, String dataType, String minDate, String maxDate) {
+		return getData(carrierName,dataType,minDate,maxDate,RequestController.DEFAULT_MIN_TIME,RequestController.DEFAULT_MAX_TIME);
+	}
+	public ArrayList<VisualParams> getData(String carrierName, String dataType) {
+		return getData(carrierName,dataType,RequestController.DEFAULT_MIN_DATE,RequestController.DEFAULT_MAX_DATE);
 	}
 }
